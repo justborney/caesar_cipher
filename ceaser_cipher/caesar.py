75432c55a68a0739ae0e0b38d32f63ffffaeb3a0
just born <justborney@gmail.com>
2023-06-13 09:58:42 +0500

    upd: comments

--- ceaser_cipher/caesar.py
+++ ceaser_cipher/caesar.py
@@ -1,11 +1,10 @@
 from string import ascii_lowercase
 from string import ascii_uppercase
 
-ascii_alphabet_length = len(ascii_lowercase)
 rus_lowercase = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
 rus_uppercase = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
-rus_alphabet_length = len(rus_lowercase)
 
+# All available characters for encryption and decryption
 available_character_set = (
     ascii_lowercase + ascii_uppercase + rus_lowercase + rus_uppercase
 )
@@ -30,16 +29,26 @@ def encryption_caesar(text: str, shift: int) -> str:
 
     for char in text:
         if char.isalpha():
+            # Determine the offset alphabet based on the case
             if char.isupper() and char in available_character_set:
                 offset = rus_uppercase if char in rus_uppercase else ascii_uppercase
             elif char.islower() and char in available_character_set:
                 offset = rus_lowercase if char in rus_lowercase else ascii_lowercase
 
+            # Find the index of the character in the offset alphabet
             index = offset.index(char)
+
+            # Apply the shift to the index
             shifted_index = (index + shift) % len(offset)
+
+            # Get the encrypted character from the offset alphabet
             encrypted_char = offset[shifted_index]
+
+            # Append the encrypted character to the result
             encrypted_text += encrypted_char
         else:
+            # If the character is not a letter or
+            # letter no in available character set, append it as is
             encrypted_text += char
 
     return encrypted_text
@@ -60,9 +69,10 @@ def decryption_caesar(text: str, shift: int) -> str:
         >>> decryption_caesar("Khoor, Zruog!", 3)
         'Hello, World!'
     """
-    # Дешифровка эквивалентна шифрованию с отрицательным смещением
+    # Decryption is equivalent to encryption with a negative offset
     return encryption_caesar(text, -shift)
 
 
 if __name__ == "__main__":
-    print(encryption_caesar("Abcd Абвг", -5))
+    # Example usage of encryption_caesar function
+    print(encryption_caesar("Hello, World!", 5))  # Mjqqt, Btwqi!
